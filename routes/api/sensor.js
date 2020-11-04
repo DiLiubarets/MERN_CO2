cddf63bce8d920c861144329f1853374e6547605
Dina <diana.liubarets@hotmail.com>
2020-11-04 01:49:07 -0500

    deploying

--- routes/api/sensor.js
+++ routes/api/sensor.js
@@ -7,6 +7,7 @@ let socketObject = {};
 router.post("/update", (req, res) => {
   const apiKey = req.body.apiKey;
   const value = req.body.value;
+  console.log("works")
   console.log(apiKey, value);
 
   if (socketObject[apiKey]) {
