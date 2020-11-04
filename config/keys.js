d25f62d5c6c08e381e63348cce15a7302471ab29
Dina <diana.liubarets@hotmail.com>
2020-11-03 23:38:04 -0500

    deleted keys

--- config/keys.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  dbURL: "mongodb+srv://diLiu:",
-  dbPath: "@cluster0.t7tmk.mongodb.net/",
-  secretOrKey: "Ukrayina91",
-  dbName: "note",
-  stripe: 'sk_test_51HfzDZHwLtaB4yxIiU5L176FNtYzGhDJvkDqC8xEggfIO9VsnTOVvsWBljeAsZuZVSbM02zowGYAxxK6ZCdlG76q00gxvuz6ym'
-};
