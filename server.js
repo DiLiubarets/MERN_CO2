d186c5b503c0f1f7c996aab2e48ee40685cb398f
Dina <diana.liubarets@hotmail.com>
2020-11-04 20:05:55 -0500

    deploying

--- server.js
+++ server.js
@@ -94,9 +94,9 @@ app.use("/api/sensor", sensor);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 
-  // app.get("*", (req, res) => {
-  //   res.sendFile(path.join(__dirname, "./client/build/index.html"));
-  // });
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  });
 }
 
 const server = app.listen(port, () =>
